44f0adad132a242f85880bc6f1446f6412ff5053
AlbertMosella <mosella.albert@gmail.com>
2022-06-07 15:54:51 +0200

    Added get one property controller

--- src/server/routers/propertyRouter.js
+++ src/server/routers/propertyRouter.js
@@ -4,6 +4,7 @@ const {
   deleteProperty,
   createProperty,
   editProperty,
+  getOneProperty,
 } = require("../controllers/propertyControllers");
 
 const propertyRouter = express.Router();
@@ -12,5 +13,6 @@ propertyRouter.get("/", getProperties);
 propertyRouter.delete("/:idProperty", deleteProperty);
 propertyRouter.post("/", createProperty);
 propertyRouter.put("/:idProperty", editProperty);
+propertyRouter.get("/:idProperty", getOneProperty);
 
 module.exports = propertyRouter;
